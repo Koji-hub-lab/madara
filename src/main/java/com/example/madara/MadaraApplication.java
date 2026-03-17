e963a5686dac19d4eabb4f2c84ccf9fb2366e6a6
koji_hub_lab <urichyamdjieu4@gmail.com>
2026-03-17 14:45:11 +0100

    premier commit projet madara

---
+++ src/main/java/com/example/madara/MadaraApplication.java
@@ -0,0 +1,13 @@
+package com.example.madara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/madara/MadaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.madara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
